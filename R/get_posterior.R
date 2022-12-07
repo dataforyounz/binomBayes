e7263e0e9af4cd10bde13935728cdd9c58760376
dataforyounz <taylor.rt17@gmail.com>
2022-12-07 19:35:30 +1300

    Added helper functions

--- R/get_posterior.R
+++ R/get_posterior.R
@@ -4,26 +4,52 @@
 #' @description Computes the posterior distribution given the supplied data and Beta distirbution
 #' priors.
 #'
-#' @param successes number of success from binomial process
-#' @param trials number of trials
-#' @param alpha_prior alpha parameter for Beta distribution prior. Defaults to 1.
-#' @param beta_prior beta parameter for Beta distribution prior. Defaults to 1.
+#' @param successes Number of success from binomial process
+#' @param trials Number of trials
+#' @param alpha_prior Alpha parameter for Beta distribution prior. Defaults to 1.
+#' @param beta_prior Beta parameter for Beta distribution prior. Defaults to 1.
+#' @param ci Posterior credible interval. Defaults to 95%.
 #'
 #' @export
 #'
 #' @examples
 #' get_posterior( successes = 5, trials = 10 )
-#' get_posterior( successes = 5, trials = 10, alpha_prior = 2, beta_prior = 2)
-get_posterior <- function( successes, trials, alpha_prior = 1, beta_prior = 1 )
+#' get_posterior( successes = 5, trials = 10, alpha_prior = 2, beta_prior = 2 )
+get_posterior <- function( successes, trials, alpha_prior = 1, beta_prior = 1, ci = .95 )
 {
 
+  # Error handling
+  if( length(successes) > 1 ) stop("ERROR: Vector inputs not allowed! Provide total number of successes.")
+  if( length(trials) > 1 ) stop("ERROR: Vector inputs not allowed! Provide total number of trials.")
   if( successes > trials ) stop( "ERROR: Number of successes cannot be larger than number of trials!")
 
-  # for plotting
-  theta_seq     <- ppoints( n = 1001 )
-  prior_density <- dbeta( x = theta_seq, shape1 = alpha_prior, shape2 = beta_prior )
+  # For plotting
+  #theta_seq     <- ppoints( n = 1001 )
+  #prior_density <- dbeta( x = theta_seq, shape1 = alpha_prior, shape2 = beta_prior )
 
-  alpha_post <- alpha + successes - 1
-  beta_post  <- alpha + trials - successes - 1
+  alpha_post <- alpha_prior + successes - 1
+  beta_post  <- beta_prior + trials - successes - 1
 
+  #posterior_density <- dbeta( x = theta_seq, shape1 = alpha_post, shape2 = beta_post )
+  posterior_hdi     <- compute_credible_interval( alpha = alpha_post, beta = beta_post, ci = ci )
+  posterior_stats   <- compute_posterior_stats( alpha = alpha_post, beta = beta_post )
+
+  # Outputs
+  output <- list()
+  #output$data <- y
+  #output$n_obs <- n
+  output$prior <- paste0("Beta(", alpha_prior, ", ", beta_prior, ")")
+  #output$prior_density <- data.frame( lambda = lambda_seq, density = prior )
+  output$posterior <- paste0("Beta(", alpha_post, ", ", beta_post, ")")
+  output$posterior_pars <- c( alpha = alpha_post, beta = beta_post )
+  #output$posterior_density <- data.frame( lambda = lambda_seq, density = posterior )
+  #output$posterior_pred <- data.frame( k = k_seq, density = posterior_pred )
+  output$posterior_summary <- posterior_stats
+  output$posterior_hdi     <- posterior_hdi
+
+  return( output )
 }
+
+
+
+
---
+++ R/helpers.R
@@ -0,0 +1,49 @@
+compute_credible_interval <- function( alpha, beta, ci = .95 ){
+
+  hdi <- c( ci_lower = qbeta(p = (1 - ci) / 2, shape1 = alpha, shape2 = beta),
+            ci_upper = qbeta(p = 1 - ((1 - ci) / 2), shape1 = alpha, shape2 = beta ) )
+
+  round( hdi, 3 )
+}
+
+compute_posterior_stats <- function( alpha, beta )
+{
+
+  posterior_mode   <- (alpha - 1) / (alpha + beta - 2)
+  posterior_median <- qbeta( p = .50, shape1 = alpha, shape2 = beta )
+  posterior_mean   <- alpha / (alpha + beta)
+  posterior_var    <- (alpha * beta) / (alpha + beta) ^ 2 * (alpha + beta + 1)
+  posterior_sd     <- sqrt( posterior_var )
+
+  round( c( mode     = posterior_mode,
+            median   = posterior_median,
+            mean     = posterior_mean,
+            variance = posterior_var,
+            std_dev  = posterior_sd ), 3)
+}
+
+# bayes_summary <- function( fit )
+# {
+#
+#   cat( paste0(
+#     "--------------------------------------------\n",
+#     "Poisson-Gamma Model\n--------------------------------------------\n",
+#     "Data",
+#     "\nObs: ", fit$n_obs,
+#     "\nSum: ", sum( fit$data ),
+#     "\nMLE: ", mean( fit$data ),
+#     "\n\nDistributions",
+#     "\nPrior:\t   ", fit$prior,
+#     "\nPosterior: ", fit$posterior,
+#     "\n\nPosterior Parameters",
+#     "\nMode: \t", round( fit$posterior_summary["mode"], 3 ),
+#     "\nMedian: ", round( fit$posterior_summary["median"], 3 ),
+#     "\nMean: \t", round( fit$posterior_summary["mean"], 3 ),
+#     "\nCI: \t", "[", round( fit$posterior_ci[1], 3 ), " ", round( fit$posterior_ci[2], 3 ), "]"
+#
+#   ))
+#
+# }
+
+
+
